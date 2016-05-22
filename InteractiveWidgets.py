0bb95e4a2a957c46238b9851a712786f3f6eba60
laserkelvin <kin.long.kelvin.lee@gmail.com>
2016-05-22 10:04:19 +1000

    Changes to indentation
    Changed from tab indents to space! Changed it to comply with python
    standards a little better.
    
    Also added comments to the InteractiveWidgets code
    
    Improving interface between InteractiveWidgets and SpectralAnalysis

--- InteractiveWidgets.py
+++ InteractiveWidgets.py
@@ -10,17 +10,42 @@ from IPython.display import display
 
 plt.style.use("fivethirtyeight")
 class PlotContainerGUI:
-    """ Trying to figure out how to re-use figure space..."""
+    """ PlotContainerGUI
+
+    An object that contains meta-objects that cam
+        be used as a general pyplot interface with 
+        interactive widgets to control the plotting 
+        styles.
+
+        Only requires initial input of a Pandas dataframe,
+        everything should be self-explanatory from then
+        onwards.
+
+        Two sub-classes are used in PlotContainerGUI:
+        1. FigureSetup
+            Container with the general figure settings
+            like style sheet and axis labels.
+        2. ClassReferences
+            Objects that will control the plot settings
+            for individual plots. This is a child of the
+            object MainTabs, which houses each ClassReference 
+            object as tabs.
+
+        General way this was written is 
+    """
     def __init__(self, DataFrame):
-        self.Figure = None
         self.DataFrame = DataFrame
+        # Generate a list of what the tabs will be called
         self.PlotTabs = [str(Key) for Key in DataFrame.keys()]
+        # For each tab, generate a PlotSettings object (tab menu objects)
         self.ClassReferences = [self.PlotSettings(Key) for Key in self.PlotTabs]
+        # Generate a list of references to the tabs
         self.TabReferences = [Key.SettingsContainer for Key in self.ClassReferences]
+        # Populate the tab menu with tabs
         self.MainTabs = widgets.Tab(children=self.TabReferences)
         self.FigureSetup = self.FigureSettings()
-        self.PlotSettings = dict()
-        self.DatatoPlot = dict()
+        self.PlotSettings = dict()                # Holds the 
+        self.DatatoPlot = dict()                  # Dictionary of the actual plots
         display(self.MainTabs)
         display(self.FigureSetup.Container)
         self.InitialisePlots()
@@ -41,6 +66,12 @@ class PlotContainerGUI:
         plt.style.use(self.Settings["Style"])
             
     def InitialisePlots(self):
+        """ Method for plotting data. This will reference a figure
+            called "Main", and generate plots in that figure.
+
+            Before the plotting is done, the figure settings are
+            retrieved from FigureSetup, called in UpdateSettings.
+        """
         plt.figure("Main", figsize=(12,8))
         self.UpdateSettings()
         for Key in self.ClassReferences:
@@ -56,6 +87,17 @@ class PlotContainerGUI:
         plt.legend()
         
     def UpdatePlot(self, Blank):
+        """ This method is called each time
+            the Update Plot button is clicked.
+
+            It sets the current figure to "Main",
+            clears it and re-plots with the latest
+            figure settings.
+
+            After plotting, it checks if the PlotBoolean
+            checkbox is clicked; if it is then we make the
+            plot visible. Otherwise, we make it invisible.
+        """
         plt.figure("Main")
         plt.clf()
         self.InitialisePlots()
@@ -93,12 +135,16 @@ class PlotContainerGUI:
                                                     self.PlotTitle
                                                    ],
                                           padding=0)
+
         def GetSettings(self):
+            """ Method for retrieving a dictionary
+                of all of the plot settings.
+            """
             self.Settings = dict()
             for Setting in self.__dict__:
                 try:
                     self.Settings[Setting] = self.__dict__[Setting].value
-                except AttributeError:
+                except AttributeError:         # Ignore what we can't get!
                     pass
             return self.Settings
     
@@ -107,8 +153,8 @@ class PlotContainerGUI:
         def __init__(self, Key):
             Types = Line2D.filled_markers
             self.DataReference = Key
-            self.Name = widgets.Text(description="Label",
-                                     value=Key)
+            self.Name = widgets.Text(description="Label",         # This only changes
+                                     value=Key)                   # the plot label!
             self.PlotColour = widgets.Dropdown(description="Plot Colour",
                                                options=["red",
                                                         "blue",
